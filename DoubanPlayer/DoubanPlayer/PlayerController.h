676c0705087bd547b629a1be3fcca1c53ec1da2e
lkjy <2748517089@qq.com>
2016-05-04 21:10:24 +0800

    li

---
+++ DoubanPlayer/DoubanPlayer/PlayerController.h
@@ -0,0 +1,13 @@
+//
+//  PlayerController.h
+//  DoubanPlayer
+//
+//  Created by lkjy on 16/5/4.
+//  Copyright © 2016年 yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayerController : MPMoviePlayerController
+
+@end
---
+++ DoubanPlayer/DoubanPlayer/PlayerController.m
@@ -0,0 +1,13 @@
+//
+//  PlayerController.m
+//  DoubanPlayer
+//
+//  Created by lkjy on 16/5/4.
+//  Copyright © 2016年 yuan. All rights reserved.
+//
+
+#import "PlayerController.h"
+
+@implementation PlayerController
+
+@end
