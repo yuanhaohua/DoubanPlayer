1ccb428f35b05925ec099703637d0ab115b2c23e
lk <746401552@qq.com>
2016-05-05 20:13:42 +0800

    阳哥创立了DMFRecChannelsEntity

---
+++ DoubanPlayer/DFMRecChannelsEntity.m
@@ -0,0 +1,13 @@
+//
+//  DFMRecChannelsEntity.m
+//  DoubanPlayer
+//
+//  Created by lk on 16/5/5.
+//  Copyright © 2016年 yuan. All rights reserved.
+//
+
+#import "DFMRecChannelsEntity.h"
+
+@implementation DFMRecChannelsEntity
+
+@end
