ccda68c62a287b05758f44da0c8a861846e90ea3
yuanhaohua <yuanhaohua123@qq.com>
2016-05-06 10:31:13 +0800

    yuan+ 构图

---
+++ DoubanPlayer/DoubanPlayer/ChannelsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ChannelsTableViewCell.h
+//  DoubanPlayer
+//
+//  Created by yuan on 16/5/6.
+//  Copyright © 2016年 yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChannelsTableViewCell : UITableViewCell
+
+@end
---
+++ DoubanPlayer/DoubanPlayer/ChannelsTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ChannelsTableViewCell.m
+//  DoubanPlayer
+//
+//  Created by yuan on 16/5/6.
+//  Copyright © 2016年 yuan. All rights reserved.
+//
+
+#import "ChannelsTableViewCell.h"
+
+@implementation ChannelsTableViewCell
+
+- (void)awakeFromNib {
+    
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
